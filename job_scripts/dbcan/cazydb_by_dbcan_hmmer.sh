42195835ae602e47b9989035aa892249ce8a2e64
Rory M Flynn <rmflynn@colostate.com>
2021-06-18 08:57:41 -0600

    Added dbcan, fixed hmmer

--- job_scripts/dbcan/cazydb_by_dbcan_hmmer.sh
+++ job_scripts/dbcan/cazydb_by_dbcan_hmmer.sh
@@ -3,19 +3,18 @@
 #SBATCH --ntasks=2
 #SBATCH --mem=8gb
 #SBATCH --time=240:00:00
-#SBATCH --job-name=shale_hmmer
+#SBATCH --job-name=cazydb_by_dbcan_hmmer
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=rmflynn@colostate.edu
 #SBATCH --partition=wrighton-hi,wrighton-low
-
 
 eval "$(conda shell.bash hook)"
 
 conda activate DRAM
 
 python /home/projects/DRAM/hmmer_mmseqs2_testing_take_3/src/hmmer.py \
+ 	--output_dir "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/results/cazydb_by_dbcan/hmmer/" \
+ 	--protein_data "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/data/dbcan/CAZyDB.07312020.fa" \
 	--hmm "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/data/dbcan/dbCAN-HMMdb-V9.txt" \
- 	--genes_faa "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/data/dbcan/CAZyDB.07312020.fa" \
- 	--output_dir "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/results/cazydb_by_dbcan/hmmer" \
- 	--name 'dbcan_hmmer' \
- 	--clock_run False
+ 	--name 'cazydb_by_dbcan_hmmer' \
+ 	--clock_run Flase
--- job_scripts/dbcan/cazydb_by_dbcan_mmseqs.sh
+++ job_scripts/dbcan/cazydb_by_dbcan_mmseqs.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=32
 #SBATCH --mem=10gb
 #SBATCH --time=1-00:00:00
-#SBATCH --job-name=dbcan_mmseqs
+#SBATCH --job-name=cazydb_by_dbcan_mmseqs
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=rmflynn@colostate.edu
 #SBATCH --partition=wrighton-hi,wrighton-low
@@ -12,9 +12,9 @@ eval "$(conda shell.bash hook)"
 
 conda activate DRAM
 
-python mmseqs.py --threads 32 \
-	--working_dir "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/results/cazydb_by_dbcan/mmseqs/"
+python /home/projects/DRAM/hmmer_mmseqs2_testing_take_3/src/mmseqs.py --threads 32 \
+	--output_dir "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/results/cazydb_by_dbcan/mmseqs/" \
 	--protein_data "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/data/dbcan/CAZyDB.07312020.fa" \
-	--raw_algs "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/data/dbcan/dbCAN-fam-aln-V9.tar.gz" \
-	--name "dbcan"\
+	--raw_aln "/home/projects/DRAM/hmmer_mmseqs2_testing_take_3/data/dbcan/dbCAN-fam-aln-V9.tar.gz" \
+	--name "cazydb_by_dbcan_mmseqs" \
 	--clock_run False
